efff8834421b15cf96eb785918326474a9c58611
saku-exe <65334585+saku-exe@users.noreply.github.com>
2020-05-19 17:11:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,3 +27,35 @@ async def on_message(message):
             await message.channel.send('はいはいわかりましたよ～')
         else:
             await message.channel.send('何様のつもり？')
+            
+            # ########################################################################
+ID_CHANNEL_WELCOME =710392333971095553  # 入室用チャンネルのID(int)
+ID_ROLE_WELCOME =  710392333971095553# 付けたい役職のID(int)
+EMOJI_WELCOME = ':baby_bottle:' # 対応する絵文字
+
+# 役職を付与する非同期関数を定義
+async def grant_role(payload):
+    # 絵文字が異なる場合は処理を打ち切る
+    if payload.emoji.name != EMOJI_WELCOME: 
+        return
+
+    # チャンネルが異なる場合は処理を打ち切る
+    channel = client.get_channel(payload.channel_id)
+    if channel.id != ID_CHANNEL_README:
+        return
+
+    # Member オブジェクトと Role オブジェクトを取得して役職を付与
+    member = channel.guild.get_member(payload.user_id)
+    role = guild.get_role(ID_ROLE_WELCOME)
+    await member.add_roles(role)
+    return member
+
+# リアクション追加時に実行されるイベントハンドラを定義
+@client.event
+async def on_raw_reaction_add(payload):
+    # 役職を付与する非同期関数を実行して Optional[Member] オブジェクトを取得
+    member = await grant_role(payload)
+    if member is not None: # 役職を付与したメンバーがいる時
+        text = f'{member.mention} ようこそ！'
+        await message.channel.send(text)
+            
